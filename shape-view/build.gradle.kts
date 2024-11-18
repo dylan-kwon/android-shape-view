bb6f692aa58e8419dca4430bb7bf430e3e248da8
dylan-kwon <dylan.kwon98@gmail.com>
2024-11-18 15:03:25 +0900

    Remove android.publishing in shape-view gradle

--- shape-view/build.gradle.kts
+++ shape-view/build.gradle.kts
@@ -30,11 +30,6 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
     dataBinding {
         enable = true
     }
